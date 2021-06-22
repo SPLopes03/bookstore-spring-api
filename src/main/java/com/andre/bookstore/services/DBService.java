b05144e19d0c7fe23818866d956090494c5ba7f7
AndreLps <andrelopes.spdia@gmail.com>
2021-06-22 17:42:53 -0300

    perfil de teste implementado

---
+++ src/main/java/com/andre/bookstore/services/DBService.java
@@ -0,0 +1,37 @@
+package com.andre.bookstore.services;
+
+import com.andre.bookstore.domain.Categoria;
+import com.andre.bookstore.domain.Livro;
+import com.andre.bookstore.repositories.CategoriaRepository;
+import com.andre.bookstore.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public void instanciaDados() {
+        Categoria cat1 = new Categoria(null, "Informatica", "livros de TI");
+        Categoria cat2 = new Categoria(null, "Ficção Cientifica", "livros de ficção");
+        Categoria cat3 = new Categoria(null, "Biografia", "livros de biografia");
+        Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+        Livro l2 = new Livro(null, "Godzilla", "Alguem ai", "Lorem ipsum", cat2);
+        Livro l3 = new Livro(null, "Stephen Hawking biografics", "Stephen Hawking", "Lorem ipsum", cat3);
+        Livro l4 = new Livro(null, "The Witcher", "Ubisoft", "Lorem ipsum", cat2);
+
+        cat1.getLivros().addAll(Arrays.asList(l1));
+        cat2.getLivros().addAll(Arrays.asList(l2, l4));
+        cat3.getLivros().addAll(Arrays.asList(l3));
+
+        this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4));
+    }
+}
