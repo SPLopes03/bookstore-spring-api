9263d6439723fce45bc3e23762eb053b99ec9a7d
AndreLps <andrelopes.spdia@gmail.com>
2021-06-22 16:34:34 -0300

    classes de domain

---
+++ src/main/java/com/andre/bookstore/domain/Categoria.java
@@ -0,0 +1,68 @@
+package com.andre.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+    private String desc;
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(Integer id, String nome, String desc) {
+        this.id = id;
+        this.nome = nome;
+        this.desc = desc;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
